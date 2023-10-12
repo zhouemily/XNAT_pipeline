c9aa05092b8b7f5e1376f45c9efeb9a0950c3d5e
Emily Zhou <40789206+zhouemily@users.noreply.github.com>
2023-10-12 06:15:43 -0700

    Update get_csv.py
    the sample csv file has two rows, 
    columns are keys

--- testdir/get_csv.py
+++ testdir/get_csv.py
@@ -15,21 +15,41 @@ csv_file_path = 'PipelineOutputs/bids/derivatives/xcp/ses-A/xcp_minimal_aroma/'+
 if DEBUG:
   print('DEBUG: csv_file_path='+csv_file_path)
 
+"""
 if os.path.exists(csv_file_path):
     # The CSV file exists
     # Proceed to open and extract data
 else:
     print(f"The CSV file '{csv_file_path}' does not exist.")
+"""
 
-if os.path.exists(csv_file_path):
-    # The CSV file exists
+######################################################################
+#In this code:
+#1. open the CSV file and read it using the csv module.
+#2. use the next(csvreader) function to read the first row (header) and store it in the header variable.
+#3. use next(csvreader) again to read the second row (data) and store it in the data variable.
+#4. create key-value pairs by zipping the header and data lists together and convert them into a dictionary, data_dict.
+#5. Finally, print the key-value pairs extracted from the CSV file.
+###########################################
+
+#
+# Initialize an empty dictionary to store the key-value pairs
+data_dict = {}
+
+# Check if the CSV file exists
+try:
     with open(csv_file_path, 'r', newline='') as csvfile:
-        csvreader = csv.DictReader(csvfile)
-        
-        for row in csvreader:
-            # Access the values by column headers
-            key = row['Column1']  # Replace 'Column1' with your actual column name
-            value = row['Column2']  # Replace 'Column2' with your actual column name
-            
-            # Process the key and value as needed
-            print(f"Key: {key}, Value: {value}")
+        csvreader = csv.reader(csvfile)      
+        # Read the header row (first row) to get column names (keys)
+        header = next(csvreader)
+        # Read the data row (second row) to get values
+        data = next(csvreader)
+        # Create key-value pairs and store them in the dictionary
+        data_dict = dict(zip(header, data))    
+except FileNotFoundError:
+    print(f"The CSV file '{csv_file_path}' does not exist.")
+except StopIteration:
+    print("The CSV file is empty or missing data.")
+# Print the extracted key-value pairs
+for key, value in data_dict.items():
+    print(f"Key: {key}, Value: {value}")
