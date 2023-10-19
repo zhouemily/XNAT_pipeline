87782d181b6b3e21b92021bd7d01ae9f93fbf051
Emily Zhou <40789206+zhouemily@users.noreply.github.com>
2023-10-18 19:08:31 -0500

    add run_xnat_pipeline.sh
    add a shell script which will run XNAT_file_pipeline.py with pre-selected cups_ids.  
    This is a wrapper script to run XNAT_file_pipeline.py

---
+++ run_xnat_pipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check for the correct number of arguments
+if [ "$#" -ne 0 ]; then
+    echo "Usage: $0"
+    exit 1
+fi
+
+# Define an array of strings
+strings=("sub-CUPS003" "sub-CUPS004" "sub-CUPS005" "sub-CUPS006" "sub-CUPS007")
+
+# Loop through the array and run the Python script with each string
+for string_arg in "${strings[@]}"; do
+    echo "Running with argument: $string_arg"
+    python XNAT_file_pipeline.py -id "$string_arg"
+done
+
