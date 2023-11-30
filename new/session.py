b895671f772153c914db0377dc5c5b26d05d951b
Emily Zhou <40789206+zhouemily@users.noreply.github.com>
2023-11-30 05:18:25 -0600

    update session.py

--- new/session.py
+++ new/session.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
+
+import netrc
 import xnat
 
-# Connect to XNAT instance
-with xnat.connect('http://your.xnat.server', use_netrc=True) as session:
-    # Your code here, for example, list all projects
+def get_credentials(host):
+    auth = netrc.netrc().authenticators(host)
+    if auth is not None:
+        return auth[0], auth[2]  # Returns login and password
+    else:
+        raise Exception(f"Credentials for '{host}' not found in .netrc")
+
+# Replace with your XNAT server's host
+host = 'xnat1.beckman.illinois.edu'
+username, password = get_credentials(host)
+
+# Connect to the XNAT server
+with xnat.connect(f'http://{host}', user=username, password=password) as session:
+    # Your code here, e.g., list all projects
     projects = session.projects
     for project in projects:
         print(project)
-
-# The connection will be automatically closed when exiting the 'with' block
 
